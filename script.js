83b24fa708c0bc624a0f16ba1bed34720665d280
ja0001 <sam.mesquita01@gmail.com>
2025-06-10 10:55:33 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function adicionarTarefa02() {
+    const mensagem = "voçê apertou o botão";
+    document.getElementById("mensagem01").textContent = mensagem;
+alert("A trilha sonora foi composta pelo próprio Toby Fox e é muito amada pelos fãs");
+}
+
+
+function adicionarTarefa03() {
+    const mensagem = "voçê apertou o botão";
+    document.getElementById("mensagem04").textContent = mensagem;
+alert("Você salva todos. Os monstros saem do subterrâneo e vão para a superfície. A criança pode escolher ficar com Toriel");
+}
